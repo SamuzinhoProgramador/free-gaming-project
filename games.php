f18c6e9715d0945886d0f87f61e24ada56cca53d
SamuzinhoProgramador <samuelbomfimbordin@gmail.com>
2026-07-09 18:37:01 -0300

    final-fixing 09.07.26

--- games.php
+++ games.php
@@ -16,6 +16,7 @@ if (!isset($_SESSION['autenticado'])){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css"> <title>Free Games</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="icon" href="img/controle.png" type="image/png"> </head>
 <body>
   <?php 
