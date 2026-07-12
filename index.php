1612aed077a117822d73b21add40188af3e75f01
SamuzinhoProgramador <samuelbomfimbordin@gmail.com>
2026-07-12 16:52:34 -0300

    resise font

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="icon" href="img/controle.png" type="image/png">
+  <style>
+    .esplicação{
+      font-size: 25px;
+    }
+  </style>
   <title>Free Games</title>
 </head>
 <body>
@@ -19,10 +24,10 @@
     include("cabecalho.php");
   ?>
   <div style="height: 200px;">
-  <p>"Olá! Bem-vindo ao meu portfólio. Este site foi criado por <a href="https://github.com/SamuzinhoProgramador">SamuzinhoProgramador</a>,</p>
-  <p>com a colaboração de <a href="https://github.com/devc-a">devc-a</a>, para expor os projetos e jogos em que temos trabalhado juntos.</p>
-  <p>Tudo o que você vê por aqui foi desenvolvido com muito PHP, HTML, CSS e JavaScript.</p>
-  <p> Fique à vontade para explorar!"</p>
+  <p class=esplicação>"Olá! Bem-vindo ao meu portfólio. Este site foi criado por <a href="https://github.com/SamuzinhoProgramador">SamuzinhoProgramador</a>,</p>
+  <p class=esplicação>com a colaboração de <a href="https://github.com/devc-a">devc-a</a>, para expor os projetos e jogos em que temos trabalhado juntos.</p>
+  <p class=esplicação>Tudo o que você vê por aqui foi desenvolvido com muito PHP, HTML, CSS e JavaScript.</p>
+  <p class=esplicação> Fique à vontade para explorar!"</p>
   </div>
   <?php include("rodapé.php"); ?>
   </body>
